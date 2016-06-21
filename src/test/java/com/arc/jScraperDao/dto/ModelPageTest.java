e7156b16ff163043b2e867c833bbbf36f753ee22
Mohd Danish <danishm026@gmail.com>
2016-06-21 16:13:29 +0530

    Created ModelPageTest.java

---
+++ src/test/java/com/arc/jScraperDao/dto/ModelPageTest.java
@@ -0,0 +1,49 @@
+package com.arc.jScraperDao.dto;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ModelPageTest {
+	@Mock
+	private ArrayList<String> mockedList;
+	
+	@Test
+	public void getAndSetStartingImageNumberTest() {
+		ModelPage page = new ModelPage();
+		int testStartingImageNumber = 1;
+		page.setStartingImageNumber(testStartingImageNumber);		
+		assertEquals(testStartingImageNumber, page.getStartingImageNumber());
+	}
+	
+	@Test
+	public void getAndSetLastImageNumberTest() {
+		ModelPage page = new ModelPage();
+		int testLastImageNumber = 71;
+		page.setLastImageNumber(testLastImageNumber);		
+		assertEquals(testLastImageNumber, page.getLastImageNumber());
+	}
+	
+	@Test
+	public void getAndSetThumbnailsListTest() {
+		ModelPage page = new ModelPage();
+		
+		page.setThumbnailsList(mockedList);
+		assertEquals(mockedList, page.getThumbnailsList());
+		
+	}
+	
+	@Test
+	public void getAndSetImagePageLinkListTest() {
+		ModelPage page = new ModelPage();
+		
+		page.setImagePageLinkList(mockedList);
+		assertEquals(mockedList, page.getImagePageLinkList());
+		
+	}
+}
